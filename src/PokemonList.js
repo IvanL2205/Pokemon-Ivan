18619cdf37f367e2c0387a77c1bb1c2c55aa9ecd
darcinmilena.becerra.contractor.cidentity <darcinmilena.becerra.contractor.cidentity@bbva.com>
2024-10-28 17:24:14 -0500

    Cambios y ajustes

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -22,7 +22,6 @@ export class PokemonList extends LitElement {
     this.title = 'Lista Pokemones';
     this.pokemonList = [];
     this.loading = false; 
-    this.fetchPokemonData();
   }
 
   static get styles() {
@@ -32,39 +31,7 @@ export class PokemonList extends LitElement {
     ];
   }
 
-  async fetchPokemonData() {
-    try {
-      
-      this.loading = true; 
-
-      const response = await fetch(
-        'https://pokeapi.co/api/v2/pokemon?offset=0&limit=50'
-      );
-      const data = await response.json();
-
-      const detailedData = await Promise.all(
-        data.results.map((pokemon) =>
-          fetch(pokemon.url).then((res) => res.json())
-        )
-      );
-
-      const basePokemon = await Promise.all(
-        detailedData.map(async(pokemon) => {
-          const speciesResponse = await fetch(pokemon.species.url);
-          const speciesData = await speciesResponse.json();
-          return speciesData.evolves_from_species ? null : pokemon;
-        })
-      );
-
-      this.pokemonList = basePokemon.filter((pokemon) => pokemon !== null);
-      console.log(this.pokemonList);
-    } catch (error) {
-      console.error('Error fetching Pokémon data:', error);
-    }
-    finally {
-      this.loading = false; 
-    }
-  }
+  
 
   render() {
     return html`
