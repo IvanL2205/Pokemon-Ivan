cbad94f1b86e505fe1579b49e5202ac0be6dbd01
darcinmilena.becerra.contractor.cidentity <darcinmilena.becerra.contractor.cidentity@bbva.com>
2024-10-28 23:23:25 -0500

    Cambios y ajustes

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
 import { getComponentSharedStyles } from '@bbva-web-components/bbva-core-lit-helpers';
 import styles from './pokemon-list.css.js';
 import '@cells-demo/demo-app-template/demo-app-template.js';
@@ -7,94 +7,89 @@ import '@bbva-experience-components/bbva-button-default/bbva-button-default.js';
 import '@bbva-web-components/bbva-web-card-product/bbva-web-card-product.js';
 import '@bbva-experience-components/bbva-type-text/bbva-type-text.js';
 import '@bbva-web-components/bbva-foundations-spinner/bbva-foundations-spinner.js';
-import '@pokemones/pokemon-dm/pokemon-dm.js'
+import '@pokemones/pokemon-dm/pokemon-dm.js';
 
 export class PokemonList extends LitElement {
   static get properties() {
     return {
       pokemonList: { type: Array },
       title: { type: String },
+      loading: { type: Boolean },
     };
   }
 
   constructor() {
     super();
-    this.title = 'Lista Pokemones';
+    this.title = 'Lista de Pokemones';
     this.pokemonList = [];
-    this.loading = false; 
+    this.loading = false;
   }
 
-  async firstUpdated(){
+  async firstUpdated() {
     const pokemonDm = this.shadowRoot.querySelector('pokemon-dm');
-    this.pokemon = await pokemonDm.fetchPokemon();
+    this.pokemonList = await pokemonDm.fetchPokemonData();
   }
 
   static get styles() {
     return [
       styles,
-      getComponentSharedStyles('pokemon-list-shared-styles'),
+      getComponentSharedStyles('PokemonList-shared-styles'),
+      css`
+        .container {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+        }
+        .card {
+          background: white;
+          border-radius: 8px;
+          box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+          margin: 10px;
+          overflow: hidden;
+          width: 150px;
+          text-align: center;
+        }
+        .card img {
+          width: 100px;
+        }
+        .card p {
+          font-size: 0.9em;
+          color: #555;
+          margin: 8px 0;
+        }
+        .card .pokemon-name {
+          font-size: 1.2em;
+          font-weight: bold;
+          color: #333;
+        }
+      `,
     ];
   }
 
-  
+  handleDetails(id) {
+    console.log(`Detalles del Pokémon con ID: ${id}`);
+  }
 
   render() {
     return html`
-      <div class="content">
-        ${this.loading
-          ? html`
-              <div class="spinner">
-                <bbva-foundations-spinner></bbva-foundations-spinner>
-              </div>
-            `
-          : html`
-          <bbva-type-text
-                          class="pokemon-title"
-                          slot="title"
-                          text="${this.title}"
-                          size="L"
-                        ></bbva-type-text>
-              <div class="container-main">
-                ${this.pokemonList.map(
-                  (pokemon) => html`
-                    <div class="pokemon-container">
-                      <div class="pokemon-card">
-                      <!-- Imagen pokemon --!>
-                        <img
-                          class="pokemon-image"
-                          slot="media"
-                          src="${pokemon.sprites.front_default}"
-                          alt="${pokemon.name}"
-                        />
-                        <!-- Nombre pokemon --!>
-                        <bbva-type-text
-                          class="pokemon-name"
-                          slot="title"
-                          text="${pokemon.name}"
-                          size="L"
-                        ></bbva-type-text>
-
-                        <!-- Tipo pokemon --!>
-                        <div class="pokemon-type" slot="details">
-                          ${pokemon.types.map(
-                            (typeInfo) => html`<span>${typeInfo.type.name}</span>`
-                          )}
-                        </div>
-                      </div>
-                      <bbva-button-default
-                        @click=${this.goToEvolution}
-                        class="evolutions-button"
-                        text="Evoluciones"
-                      ></bbva-button-default>
-                    </div>
-                  `
-                )}
-              </div>
-            `}
+      <h1>${this.title}</h1>
+      <div class="container">
+        ${this.pokemonList.map(
+          (pokemon) => html`
+            <div class="card">
+              <img src="${pokemon.image}" alt="${pokemon.name}" />
+              <p class="pokemon-name">${pokemon.name}</p>
+              <p>${pokemon.abilities}</p>
+              <bbva-button-default
+                text="Details"
+                @click="${() => this.handleDetails(pokemon.id)}">
+              </bbva-button-default>
+            </div>
+          `
+        )}
       </div>
       <pokemon-dm></pokemon-dm>
     `;
   }
-  
-
 }
+
